d9770fb3f4ef9ef6988fdefdce1c6fcb5062a1ce
Alex Camacho <camachoalex1k@gmail.com>
2023-06-30 16:30:48 -0500

    agregando configuracion del server

--- server/lib/database/database.js
+++ server/lib/database/database.js
@@ -2,7 +2,6 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-
 // Configura con tus credenciales propios
 //export const sequelize = new Sequelize("alimentos", "postgres", "admin", {
 //   host: "localhost",
@@ -15,8 +14,5 @@ export const sequelize = new Sequelize("railway", "postgres", "Fmb3lTnosaU6An63a
   host: "containers-us-west-46.railway.app",
   logging: false,
   dialect: "postgres",
-  port: 7537,
+  port: 7537
 });
-
-
-
