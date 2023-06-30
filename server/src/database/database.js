f30093b84a83fb78cfccf8e3f3f21150ba2fe214
erikstiven <105946950+erikstiven@users.noreply.github.com>
2023-06-30 07:21:00 -0500

    version personal

--- server/src/database/database.js
+++ server/src/database/database.js
@@ -2,9 +2,15 @@ import Sequelize from "sequelize";
 
 // conexion a la base de datos
 
-export const sequelize = new Sequelize("recetasdb", "postgres", "backend", {
+export const sequelize = new Sequelize(
+  "recetasdb", //nombre de la base de datos
+  "postgres",  //user
+  "Nasa4036",   //password
+{
   host: "localhost",
+  logging: false,
   dialect: "postgres",
+ 
 });
 
 
