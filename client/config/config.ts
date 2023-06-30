b9ca53735f9e6c05ca6ed3c6f96a3c605477a2c5
Alex Camacho <camachoalex1k@gmail.com>
2023-06-30 16:57:52 -0500

    nuevas modificaciones2

--- client/config/config.ts
+++ client/config/config.ts
@@ -2,7 +2,6 @@ import { environment } from 'src/environments/environment';
 
 export default {
   URL_BASE: environment.url,
-   URL_BASE_PATH: environment.baseUrl,
    URL_API_BASE: environment.urlApi,
 //    URL_MAIL_SERVICE: environment.urlOneDriveService,
 //   CODIGO_SISTEMA_ONEDRIVE: environment.CodigoSistemaOneDrive,
